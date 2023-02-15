a30287c73ffadf5706792b0da51462c7529c054f
ramonlopezb <ramonlb54@gmail.com>
2023-02-15 19:07:01 -0300

    variables globales,funtion destapar,aciertos
    declaracion variables globales, comienzo function destapar, creacion de contador de tarjetas destapadas,mostrar primera tarjeta, mostrar segunda tarjeta,deshabilitacion de tarjetas,deshabilitacion del primer boton y segundo boton

--- Proyect Game/juego.js
+++ Proyect Game/juego.js
@@ -1,3 +1,44 @@
+//generacion de variables//
+let tarjetasDestapadas = 0
+let tarjeta1=null
+let tarjeta2=null
+let primerResultado= null
+let segundoResultado=null
+let aciertos=0
+// apuntando a documento HTML//
+let mostrarAciertos=document.getElementById(aciertos)
+//generacion de numeros aleatorios//
 let numeros=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12]
 numeros=numeros.sort(()=>{return Math.random()-onabort-0.5})
 console.log(numeros)
+//funcion principal//
+function destapar(id){
+    tarjetasDestapadas++;
+    console.log(tarjetasDestapadas)
+
+    if(tarjetasDestapadas == 1){
+        //mostrar primer numero//
+        tarjeta1 = document.getElementById(id)
+        primerResultado = numeros[id]
+        tarjeta1.innerHTML = primerResultado
+
+        //deshabilitar primer boton//
+        tarjeta1.disabled=true
+    }else if(tarjetasDestapadas == 2){
+       //mostrar segundo numero
+       tarjeta2= document.getElementById(id)
+       segundoResultado=numeros[id]
+       tarjeta2.innerHTML = segundoResultado
+       //deshabilitar segundo boton//
+       tarjeta2.disabled=true
+    }
+    if(primerResultado == segundoResultado){
+        //Encerar contador tarjetas destapadas//
+        tarjetasDestapadas= 0
+        //aumentar aciertos//
+        aciertos++
+        mostrarAciertos.innerHTML= "aciertos: ${aciertos}"
+
+    }
+   
+}
