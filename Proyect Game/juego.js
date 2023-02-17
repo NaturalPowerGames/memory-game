707286fd67dad353a9802f629ac41420b835a31b
ramonlopezb <ramonlb54@gmail.com>
2023-02-17 11:19:30 -0300

    arreglo error html
    se ajusto numero de tarjetas a 12 pares, coloco clas a los botones comenzar juego y  jugar de nuevo , asi como los colores de fondo y de letra, una mejor estetica

--- Proyect Game/juego.js
+++ Proyect Game/juego.js
@@ -6,7 +6,7 @@ let primerResultado= null
 let segundoResultado=null
 let aciertos=0
 // apuntando a documento HTML//
-let mostrarAciertos=document.getElementById(aciertos)
+let mostrarAciertos=document.getElementById("aciertos")
 //generacion de numeros aleatorios//
 let numeros=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12]
 numeros=numeros.sort(()=>{return Math.random()-onabort-0.5})
@@ -33,12 +33,25 @@ function destapar(id){
        tarjeta2.disabled=true
     }
     if(primerResultado == segundoResultado){
-        //Encerar contador tarjetas destapadas//
+        //Encerar (volver a cero contador tarjetas destapadas)//
         tarjetasDestapadas= 0
         //aumentar aciertos//
         aciertos++
-        mostrarAciertos.innerHTML= "aciertos: ${aciertos}"
-
+        mostrarAciertos.innerHTML= `Aciertos: ${aciertos}`
+        //premios para los aciertos//
+    }else{
+      setTimeout(()=>{
+        tarjeta1.innerHTML=" "
+        tarjeta2.innerHTML=" "
+        tarjeta1.disabled= false
+        tarjeta2.disabled= false
+        tarjetasDestapadas=0
+      },2000)  
     }
+    }
+  
+
+       
+        
+        
    
-}
